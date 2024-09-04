eeff58ed77acaa7470a74a916c7c272b8de27c8b
lkimwilu <lkimwilu@gmail.com>
2024-09-04 09:42:59 +0300

    variables

---
+++ variable.py
@@ -0,0 +1,40 @@
+# number = 1
+# name = "lyric"
+ 
+#  print("name")
+
+#  a, b, c = "self", 23, True
+#  print(a, b, c)
+
+# python is case sensitive
+
+
+# site1 = site2 = "Shield.intl"
+
+# print(site2)
+# site 3 = "yooh"
+# site 4 = site 3
+
+# print(site4)
+
+
+# languages = ["HTML", "JS", "PYTHON",3, True, None, 23, "Fel"]
+
+    #  print(languages)
+
+#  print(languages[-3]) 
+
+# print(languages[1:6])  
+
+# playstation = ("xBox", "controller", 400, True)
+
+# print(playstation[2])
+
+def sum():
+print(2 + 3)
+sum()
+
+def add_numbers():
+    print(2 + 3)
+
+add_numbers()
